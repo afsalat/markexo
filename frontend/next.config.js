f7c606b0d3d98947728efcfdd0ba0c3315b51cd2
afsalat <afsalat9@gmail.com>
2026-03-08 22:37:34 +0530

    init: Initialize frontend Next.js configuration and backend Django project settings.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['vorionmart.com', 'localhost', '127.0.0.1'],
+        domains: ['vorionmart.com'],
         remotePatterns: [
             {
                 protocol: 'https',
